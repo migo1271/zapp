49319edffe3971011dffb1dd6a57399ea565260b
Christine Coenen <code.coenen@gmail.com>
2025-07-08 19:26:28 +0200

    Add player error message

--- app/src/main/java/de/christinecoenen/code/zapp/tv2/player/PlayerScreen.kt
+++ app/src/main/java/de/christinecoenen/code/zapp/tv2/player/PlayerScreen.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.media3.ui.compose.PlayerSurface
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Text
 import de.christinecoenen.code.zapp.app.player.Player
 import de.christinecoenen.code.zapp.app.player.VideoInfo
 import de.christinecoenen.code.zapp.tv2.main.navigation.Location
@@ -26,6 +30,7 @@ fun PlayerScreen(
 	player: Player = koinInject<Player>(),
 ) {
 	var controllerVisible by remember { mutableStateOf(false) }
+	val error by player.errorResourceId.collectAsState(null)
 
 	BackHandler(controllerVisible) {
 		controllerVisible = false
@@ -49,6 +54,19 @@ fun PlayerScreen(
 		}
 	)
 
+	// TODO: move to own component
+	// TODO: style
+	if (error != null && error != -1) {
+		Text(
+			text = stringResource(error!!),
+			color = MaterialTheme.colorScheme.error,
+			style = MaterialTheme.typography.headlineLarge,
+		)
+	}
+
+	// TODO: show loading indicator
+
+	// TODO: hide with delay when video is playing
 	ControllerOverlay(
 		title = videoInfo.title,
 		subtitle = videoInfo.subtitle,
